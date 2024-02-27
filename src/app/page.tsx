dff91baeca39b1fc4770b41842dab8f07dfccf30
panoskouff <me@panoskouf.xyz>
2024-02-27 15:28:23 +0200

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,29 @@ import { getServerSession } from 'next-auth'
 import SessionProvider from '#/components/SessionProvider'
 import { authOptions } from './api/auth/[...nextauth]/authOptions'
 import Link from 'next/link'
+import { Center, Text } from '#/atoms'
+import { SectionContainer } from '#/components/SectionContainer'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
-  if (session) {
+  if (!session) {
     return (
-      <SessionProvider session={session}>
-        <div>homepage</div>
-      </SessionProvider>
-    )
-  } else {
-    return (
-      <div>
-        Please <Link href='/signIn'>sign in</Link> to proceed
-      </div>
+      <SectionContainer>
+        <Center h='80vh'>
+          <Text>
+            Please <Link href='/signIn'>sign in</Link> to proceed
+          </Text>
+        </Center>
+      </SectionContainer>
     )
   }
+
+  return (
+    <SessionProvider session={session}>
+      <SectionContainer>
+        <div>homepage</div>
+      </SectionContainer>
+    </SessionProvider>
+  )
 }
