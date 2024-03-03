8be418c183b368fc42d47d538b3a9881ecfefa60
panoskouff <me@panoskouf.xyz>
2024-03-03 12:13:10 +0200

    fix jest crushing due to prisma

--- src/tests/jestPostSetup.ts
+++ src/tests/jestPostSetup.ts
@@ -1,2 +1,24 @@
 import '@testing-library/jest-dom'
 import '@testing-library/jest-dom/extend-expect'
+import { PrismaClient } from '@prisma/client'
+
+jest.mock('@prisma/client', () => {
+  // const originalModule = jest.requireActual('@prisma/client')
+
+  const mockPrismaClient = {
+    user: {
+      // findUnique: jest.fn().mockResolvedValue({ /* Mocked user data */ }),
+      // create: jest.fn().mockResolvedValue({ /* Mocked user data */ }),
+      //...
+    },
+  }
+
+  return {
+    __esModule: true,
+    // ...originalModule,
+    PrismaClient: jest.fn().mockImplementation(() => mockPrismaClient),
+  }
+})
+
+// global.prisma = new PrismaClient()
+global.prisma = new PrismaClient()
